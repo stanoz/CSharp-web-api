db911985658b8645d17b06eb7cf95f2cd70cef00
stanoz <s.oziemczuk@gmail.com>
2024-04-13 13:08:37 +0200

    added Planet entity

---
+++ StarWarsWebApplication/StarWarsWebApplication/Entities/Planet.cs
@@ -0,0 +1,16 @@
+﻿namespace StarWarsWebApplication.Entities
+{
+    public class Planet
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public int Diameter { get; set; }
+        public int RotationPeriod { get; set; }
+        public int OrbitalPeriod { get; set; }
+        public int Gravity { get; set; }
+        public int Population { get; set; }
+        public IEnumerable<string>? Climate { get; set; }
+        public IEnumerable<string>? Terrain { get; set; }
+
+    }
+}
